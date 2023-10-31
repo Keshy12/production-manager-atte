fd3587183c801bb12dbb35eefc94b7eb848d2605
Keshy12 <100865662+Keshy12@users.noreply.github.com>
2023-10-31 11:14:55 +0100

    Configuration file (autoload)
    Basic configuration file. Autoload implementation.

--- config/config.php
+++ config/config.php
@@ -1,11 +1,10 @@
 <?php
-
-require_once 'C:/xampp/htdocs/atte_ms_new/vendor/autoload.php';
-require_once 'config-google-sheets.php';
-
 define("ROOT_DIRECTORY", $_SERVER['DOCUMENT_ROOT']."/atte_ms_new");
+require_once ROOT_DIRECTORY.'/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(ROOT_DIRECTORY);
-$dotenv->load();
+
+
+
+
 
 
