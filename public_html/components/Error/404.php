233671b22ea80f7d382b628b26abd21ec67dbe60
Keshy12 <100865662+Keshy12@users.noreply.github.com>
2024-01-03 12:22:16 +0100

    404 page
    Create 404.php page.

---
+++ public_html/components/Error/404.php
@@ -0,0 +1,6 @@
+<div class="d-flex align-items-center justify-content-center mt-4 bg-white">
+    <h1 class="display-1 fw-bold text-dark">404</h1>
+</div>
+<div class="d-flex align-items-center justify-content-center bg-white">
+    <h3 class="text-dark text-center w-100">Page Not Found</h3>
+</div>
