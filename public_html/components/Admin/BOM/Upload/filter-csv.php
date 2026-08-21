9998883e97f05776683bb5803cad7420285a92d5
Keshy12 <100865662+Keshy12@users.noreply.github.com>
2026-08-21 12:59:20 +0200

    fix(bom-upload): filter any value starting with DNP, not just exact matches

--- public_html/components/Admin/BOM/Upload/filter-csv.php
+++ public_html/components/Admin/BOM/Upload/filter-csv.php
@@ -2,7 +2,7 @@
 function filterCSVArray($csvArray, $packageToExclude)
 {
     $csvArray = array_filter($csvArray, function($row) {
-        return $row[1] !== "DNP" && $row[1] !== "DNP_SS520G";
+        return !str_starts_with($row[1], 'DNP');
     });
     
     $csvArray = array_filter($csvArray, function($row) use ($packageToExclude) {
